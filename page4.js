bbe9b6dc742472a396440525d9ca65b3de6d99b0
Murtada Ahmed <101924930+MurtadaFrontDev@users.noreply.github.com>
2026-02-06 02:03:34 +0300

    تحديث ثاني

--- page4.js
+++ page4.js
@@ -1,4 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.remove("not-loaded");
+
+    const envelope = document.querySelector('.envelope-wrapper');
+    envelope.addEventListener('click', () => {
+        envelope.classList.add('open');
+    }, { once: true });
 });
